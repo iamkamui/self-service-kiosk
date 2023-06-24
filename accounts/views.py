c5356c2755a2f4480ed6d4f1ae8cc9ed52c93d0a
Pedro Augusto <pagb.pedro@hotmail.com>
2023-06-24 13:44:31 -0300

    add API endpoint to views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from accounts.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows user to be vieweed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be vieweed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
