001bafd44f69ff2da15405bcbf05568420154bd5
Pedro Augusto <pagb.pedro@hotmail.com>
2023-06-27 20:13:31 -0300

    :test_tube: test: Add get one user to test

--- accounts/tests.py
+++ accounts/tests.py
@@ -22,4 +22,11 @@ class UserTestCase(APITestCase):
         
         response = self.client.get(f'/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_get_one_user(self):
+
+        for user in self.users:
+            response = self.client.get(f'/users/{user.pk}/')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
         
