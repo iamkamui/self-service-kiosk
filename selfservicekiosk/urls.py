ea1032e55efb3c2ad4f88e0439e24cf41f10fd18
Pedro Augusto <pagb.pedro@hotmail.com>
2023-06-27 20:17:51 -0300

    :sparkles: feat: Add api token auth route

--- selfservicekiosk/urls.py
+++ selfservicekiosk/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounts import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -26,6 +27,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/',  include('rest_framework.urls', namespace='rest_framework', )),
+    path('api-token-auth/', obtain_auth_token),
     path('admin/', admin.site.urls),
 ]
