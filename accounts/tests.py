2cb303b58337a7a78a2b12ccec6e8ddc39ba021e
Pedro Augusto <pagb.pedro@hotmail.com>
2023-06-27 18:57:01 -0300

    :test_tube: test: Add test to get all users

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.contrib.auth.models import User, Permission
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class UserTestCase(APITestCase):
+    """
+    Test for user
+    """
+    def setUp(self) -> None:
+        
+        self.user = User.objects.create_user(username= "admintest", password= "admintest123", email= "admin_test@admin.com")
+        self.users = User.objects.all()
+        self.token = Token.objects.get(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+    def test_get_all_users(self):
+        """
+        Test UserViewSet list method
+        """
+        
+        response = self.client.get(f'/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
