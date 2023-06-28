902b51f4d99f4b44a5cfc866b301db9ceadc8cbe
Pedro Augusto <pagb.pedro@hotmail.com>
2023-06-28 16:05:18 -0300

    Add models to showing in admin page

--- consumer/admin.py
+++ consumer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from consumer.models import Product, Category
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price', 'created_at', 'modified_at')
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'modified_at')
