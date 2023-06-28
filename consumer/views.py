f0ceab52498c17ed6e994ff79e0f00d71e102183
Pedro Augusto <pagb.pedro@hotmail.com>
2023-06-28 16:07:03 -0300

    :sparkles: feat: Add endpoint viewset to product model

--- consumer/views.py
+++ consumer/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from consumer.models import Product
+from consumer.serializers import ProductSerializer
 # Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows product to be viewed or edited.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
