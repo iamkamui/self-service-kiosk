bcbcb7a1dc1ee93c46c6792d336a9efbac565d01
Pedro Augusto <pagb.pedro@hotmail.com>
2023-06-28 16:09:44 -0300

    :bug: fix: Fixing rest framework authorization to allow web login

--- selfservicekiosk/urls.py
+++ selfservicekiosk/urls.py
@@ -18,16 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounts import views
+from consumer.views import ProductViewSet
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'products', ProductViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/',  include('rest_framework.urls', namespace='rest_framework', )),
+    path('api-auth/',  include('rest_framework.urls', namespace='rest_framework',)),
     path('api-token-auth/', obtain_auth_token),
     path('admin/', admin.site.urls),
 ]
