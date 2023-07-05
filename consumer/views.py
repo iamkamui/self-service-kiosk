7d019a80fc73f959ad82677cd4e9292dc4b7cecd
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-05 09:53:17 -0300

    :sparkles: feat: Add order endpoint

--- consumer/views.py
+++ consumer/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from consumer.models import Product
-from consumer.serializers import ProductSerializer
+from consumer.models import Product, Order
+from consumer.serializers import ProductSerializer, OrderSerializer
 # Create your views here.
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -9,4 +9,21 @@ class ProductViewSet(viewsets.ModelViewSet):
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows to create, view or edit order.
+    """
+    permission_classes = [permissions.AllowAny]
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        session = self.request.session
+        return Order.objects.filter(session = session)
+    
+    
+    def create(self, request):
+        pass
