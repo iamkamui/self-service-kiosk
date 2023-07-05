3475d5b3a4415a1f1d33595a2f7df9d0cd610e48
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-05 09:48:54 -0300

    :sparkles: feat: Add order serializer

--- consumer/serializers.py
+++ consumer/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from consumer.models import Product
+from consumer.models import Product, Order
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'url', 'name', 'category', 'price', 'created_at', 'modified_at']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(queryset= Product.objects.all(), many=False)
+    class Meta:
+        model = Order
+        fields = ['url', 'id', 'session', 'product', 'quantity']
