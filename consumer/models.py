3d565258b4d85f91771428605ae3c7b688bc0fbb
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-05 09:47:40 -0300

    :sparkles: feat: Add order model

--- consumer/models.py
+++ consumer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.sessions.models import Session
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
@@ -12,10 +13,11 @@ class Category(models.Model):
         verbose_name_plural = 'Categorys'
     
     def __str__(self) -> str:
-        return f'{self.name}'
+        return f'{self.name}  ({self.pk})'
 
 
 class Product(models.Model):
+    
     name = models.CharField(_('name'), max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(_("price"), max_digits=10, decimal_places=2)
@@ -27,3 +29,12 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}'
+    
+
+class Order(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, null=False, blank=False)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
+    quantity = models.IntegerField(_("Quantity"), default=0)
+
+    def __str__(self) -> str:
+        return f'{self.pk}'
